259feff8b2cf4ad0467a6d553dabe709c8e6864c
codingxllama <osamaalchalabi2@gmail.com>
2022-01-26 23:29:11 -0500

    Added the script for using local quotes

--- script.js
+++ script.js
@@ -1,35 +1,28 @@
 // Getting Quotes From API
+
 // Asynchronous can work at any time without having to wait on the browser
 
+let apiQuotes = [];
 
-
-// let apiQuotes = [];
-
-// Show new quote function
-
-function newQuote()
-{
-const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
-console.log(quote)
+function newQuote() {
+  // Pick a random quote from apiQuotes array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  console.log(quote);
 }
 
-
-
-// async function getQuotes() {
-//   const apiURL = "https://type.fit/api/quotes";
-//   // attempting to complete a fetch request
-//   try {
-//     //   this response const will not be populated until it has fetched some data
-//     const response = await fetch(apiURL);
-//     apiQuotes = await response.json();
-//     newQuote();
-//     // console.log(apiQuotes[12]);
-//   } catch (error) {
-//     //Catch Error goes here
-//   }
-// }
+async function getQuotes() {
+  const apiURL = "https://type.fit/api/quotes";
+  // attempting to complete a fetch request
+  try {
+    //   this response const will not be populated until it has fetched some data
+    const response = await fetch(apiURL);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    //Catch Error goes here
+  }
+}
 
 //On Load
-// getQuotes();
 
-newQuote()
+getQuotes();

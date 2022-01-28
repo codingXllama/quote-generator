68c6ddcdb0b0496c8430aacf3ac3807d1aa82131
codingxllama <osamaalchalabi2@gmail.com>
2022-01-27 22:47:16 -0500

    added dynamic quotes feature

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 // Getting Quotes From API
-
 // Asynchronous can work at any time without having to wait on the browser
 
-let apiQuotes = [];
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
 
+let apiQuotes = [];
 function newQuote() {
   // Pick a random quote from apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-  console.log(quote);
+  // Checking if the Author field in the quotes.js file is empty, if so we replace it with Unknown
+  if (!quote.author) {
+    authorText.textContent = "Unknown";
+  } else {
+    // populating the quote's author and quote string
+    authorText.textContent = quote.author;
+  }
+  // checking the quote length to determine styling
+
+  if (quote.text.length > 130) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+  quoteText.textContent = quote.text;
 }
 
 async function getQuotes() {

b314fee0d66eefe71d49301a1c8f042ca26ec92b
codingxllama <osamaalchalabi2@gmail.com>
2022-01-30 00:23:12 -0500

    Added the loader animation

---
+++ misc.js
@@ -0,0 +1,5 @@
+// function openGoogle() {
+//   const userName = "iraq284";
+//   const googleUrl = `https://www.youtube.com/user/${userName}`;
+//   window.open(googleUrl, "_blank");
+// }
--- script.js
+++ script.js
@@ -6,9 +6,27 @@ const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
 
 let apiQuotes = [];
+
+// Show loading animation
+
+function showLoader() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Hide loading animation
+
+function hideLoader() {
+  loader.hidden = true;
+  quoteContainer.hidden = false;
+}
+
 function newQuote() {
+  showLoader();
+  // loader.classList.remove("hider");
   // Pick a random quote from apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
   // Checking if the Author field in the quotes.js file is empty, if so we replace it with Unknown
@@ -19,22 +37,26 @@ function newQuote() {
     authorText.textContent = quote.author;
   }
   // checking the quote length to determine styling
-
   if (quote.text.length > 130) {
     quoteText.classList.add("long-quote");
   } else {
     quoteText.classList.remove("long-quote");
   }
+
+  // We will set the quote and hide the Loader
   quoteText.textContent = quote.text;
+  hideLoader();
 }
 
 async function getQuotes() {
+  showLoader();
   const apiURL = "https://type.fit/api/quotes";
   // attempting to complete a fetch request
   try {
     //   this response const will not be populated until it has fetched some data
     const response = await fetch(apiURL);
     apiQuotes = await response.json();
+    // showLoader();
     newQuote();
   } catch (error) {
     //Catch Error goes here
@@ -49,15 +71,11 @@ function tweetQuote() {
   window.open(twitterUrl, "_blank");
 }
 
-// function openGoogle() {
-//   const userName = "iraq284";
-//   const googleUrl = `https://www.youtube.com/user/${userName}`;
-//   window.open(googleUrl, "_blank");
-// }
-
 // EventListeners supposed to be placed at the bottom where we add the fns before calling the btn
 newQuoteBtn.addEventListener("click", newQuote);
 twitterBtn.addEventListener("click", tweetQuote);
 
 //On Load
+
+// showLoader();
 getQuotes();
